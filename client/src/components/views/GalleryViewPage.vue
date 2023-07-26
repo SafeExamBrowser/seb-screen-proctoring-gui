e1bf27aa113fba466ec7d652a436bdcd907107ca
Nadim Ritter <nadim.ritter@let.ethz.ch>
2023-07-26 17:40:17 +0200

    working demo with window slider

--- client/src/components/views/GalleryViewPage.vue
+++ client/src/components/views/GalleryViewPage.vue
@@ -1,35 +1,40 @@
 <template>
+    <v-window v-model="currentWindow" @update:model-value="windowChange()" show-arrows>
+        <v-window-item v-for="w in windowsAmount" class="padding">   
 
-    <v-row v-if="!noScreenshotData" v-for="i in GRID_SIZE">
-        <v-col v-for="n in GRID_SIZE">
+            <v-row v-if="!noScreenshotData" v-for="i in GRID_SIZE">
+                <v-col v-for="n in GRID_SIZE">
+                    <v-card v-if="currentIndexExists(calcIndex(i, n))">
+                        <v-card-title> {{returnClientName(calcIndex(i, n))}}</v-card-title>
+                        <v-img v-if="screenshots != undefined && screenshots[calcIndex(i, n)].active" :src="createImageLinkWithToken(calcIndex(i, n)) + '&t=' + timestamp"></v-img>
+                        <v-img v-else :src="createImageLinkWithToken(calcIndex(i, n))"></v-img>
 
-            <v-card v-if="currentIndexExists(calcIndex(i, n))">
-                <v-card-title> {{returnClientName(calcIndex(i, n))}}</v-card-title>
-                <v-img v-if="screenshots != undefined && screenshots[calcIndex(i, n)].active" :src="createImageLinkWithToken(calcIndex(i, n)) + '&t=' + timestamp"></v-img>
-                <v-img v-else :src="createImageLinkWithToken(calcIndex(i, n))"></v-img>
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn rounded="sm" color="primary" variant="outlined" @click="openDialog(calcIndex(i, n))">
+                                Expand
+                            </v-btn>
+                            
+                            <v-btn :to="getProctoringViewLink(calcIndex(i, n))" rounded="sm" color="primary" variant="flat">
+                                Details View
+                            </v-btn>
+                        </v-card-actions>
+                    </v-card>
+                </v-col>
+            </v-row>
 
-                <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn rounded="sm" color="primary" variant="outlined" @click="openDialog(calcIndex(i, n))">
-                        Expand
-                    </v-btn>
-                    
-                    <v-btn :to="getProctoringViewLink(calcIndex(i, n))" rounded="sm" color="primary" variant="flat">
-                        Details View
-                    </v-btn>
-                </v-card-actions>
-            </v-card>
+            <v-alert v-else color="warning" icon="$warning" title="No data available" :text="getAlertText()"></v-alert>
 
-        </v-col>
-    </v-row>
+            <v-dialog v-model="dialog" max-width="1500">
+                <v-card>
+                    <v-img :src="openedImageLink"></v-img>
+                </v-card>
+            </v-dialog>
 
-    <v-alert v-else color="warning" icon="$warning" title="No data available" :text="getAlertText()"></v-alert>
+        </v-window-item>
 
-    <v-dialog v-model="dialog" max-width="1500">
-      <v-card>
-        <v-img :src="openedImageLink"></v-img>
-      </v-card>
-    </v-dialog>
+    </v-window>
+
 
 
 
@@ -37,7 +42,7 @@
 
 
 <script setup lang="ts">
-    import {ref, onBeforeMount, onBeforeUnmount} from "vue";
+    import {ref, onBeforeMount, onBeforeUnmount, computed} from "vue";
     import {useRoute} from "vue-router";
     import * as groupService from "../../services/groupService";
     import {SortOrder} from "../../models/sortOrderEnum";
@@ -57,6 +62,9 @@
     const screenshots = ref<Screenshot[]>();
     const noScreenshotData = ref<boolean>(false);
     const timestamp = ref(Date.now());
+    const windowsAmount = ref<number>(1);
+    const currentWindow = ref<number>(0);
+    const numberOfSessions = ref<number>();
 
     let openedImageLink: string = "";
     let intervalScreenshots: any | null = null;
@@ -66,11 +74,14 @@
     onBeforeMount(async () => {
         await getAndAsingScreenshots();
         titleStore.title = "Gallery View of Group: " + groupName;
+        windowsAmount.value = calcAmountOfWindows();
         
+
         //relaod data as long as atleast one item has an active session
-        if(screenshots.value?.some(screenshot => screenshot.active)){
+        if(screenshots.value?.some(screenshot => screenshot.active) || screenshots.value == null){
 
             intervalScreenshots = setInterval(async () => {
+                console.log("test")
                 await getAndAsingScreenshots();
             }, IMG_URL_SCREENSHOTS_RELOAD_INTERVAL_IN_S);
 
@@ -78,6 +89,8 @@
                 timestamp.value = Date.now();
             }, SCREENSHOTS_RELOAD_INTERVAL_IN_S);
         }
+
+        console.log("windowsAmount: " + windowsAmount.value)
         
     });
 
@@ -93,16 +106,21 @@
 
     async function getAndAsingScreenshots(){
         try {
-            const groupUuidResponse: GroupUuidResponse = await groupService.getGroupByUuid(groupUuid, {sortOrder: SortOrder.desc});
+            const groupUuidResponse: GroupUuidResponse = await groupService.getGroupByUuid(groupUuid, {sortOrder: SortOrder.desc, pageNumber: (currentWindow.value == 0) ? 1 : currentWindow.value});
+            //todo: extract groupname
             groupName = groupUuidResponse.name;
+            groupUuidResponse.numberOfSessions
+            noScreenshotData.value = false;
 
             if(groupUuidResponse.screenshots == null || groupUuidResponse.screenshots.length == 0){
                 noScreenshotData.value = true;
                 return;
             }
 
+            //todo: maybe change object to reactive instead of ref
             // screenshots.value = groupUuidResponse.screenshots.slice(0, -4);
             screenshots.value = groupUuidResponse.screenshots.flatMap(f => f ? [f] : []);
+            numberOfSessions.value = groupUuidResponse.numberOfSessions;
 
             if(screenshots.value.length == 0){
                 noScreenshotData.value = true;
@@ -115,6 +133,19 @@
             //todo: add better error handling
             console.error(error);
         }
+    }
+
+    function calcAmountOfWindows(): number{
+        if(numberOfSessions.value == null){
+            return 1;
+        }
+
+        return Math.ceil(numberOfSessions.value / Math.pow(GRID_SIZE, 2));
+    }
+
+    async function windowChange(){
+        console.log(currentWindow.value)
+        await getAndAsingScreenshots();
     }
 
     //todo: change to computed prop
@@ -131,6 +162,7 @@
     }
 
     function returnClientName(index: number): string{
+
         if(screenshots.value != null){
             return screenshots.value[index].clientName;
         }
@@ -170,9 +202,11 @@
 </script>
 
 <style>
-    /* .gallery-view-container{
-        width: 60%;
-        height: 60%;
-    } */
+    .padding{
+        padding-top: 25px;
+        padding-left: 100px;
+        padding-right: 100px;
+        padding-bottom: 25px;
+    }
 
 </style>
