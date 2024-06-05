ddfe948bf8213db4a2799cf2a98cc182dfcee5a7
Nadim Ritter <nadim.ritter@let.ethz.ch>
2024-06-05 09:25:53 +0200

    SEBSP-136 add boolean to check if session was openedusing timestamp in live mode

--- client/src/components/views/proctoring/ProctoringViewPage.vue
+++ client/src/components/views/proctoring/ProctoringViewPage.vue
@@ -254,6 +254,7 @@
 
     //router params
     const sessionId: string = useRoute().params.sessionId.toString();
+    const searchTimestampOnLoad = ref<boolean>(false);
     const searchTimestamp: string | undefined = useRoute().query.searchTimestamp?.toString();
 
     //fullscreen
@@ -445,11 +446,15 @@
         }
         stopIntervalScreenshots();
 
-        isLiveSelected.value = true;
-        isPlaying.value = true;
-        isLiveButtonDisabled.value = true;
+        if(!searchTimestampOnLoad.value){
+            isLiveSelected.value = true;
+            isPlaying.value = true;
+            isLiveButtonDisabled.value = true;
 
-        sliderTime.value = sliderMax.value;
+            sliderTime.value = sliderMax.value;
+        }
+
+        searchTimestampOnLoad.value = false;
 
         startIntervalLiveImage();
         startIntervalRefresh();
@@ -546,6 +551,7 @@
 
     function setStartingSliderTime(){
         if(searchTimestamp){
+            searchTimestampOnLoad.value = true;
             sliderTime.value = parseInt(searchTimestamp);
         }else{
             sliderTime.value = sliderMin.value;
