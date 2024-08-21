9c09f521cdb3bf163745f150db09645a06da1d52
Nadim Ritter <nadim.ritter@let.ethz.ch>
2024-08-21 11:38:52 +0200

    add /sps-gui to main server file

--- server/src/app.ts
+++ server/src/app.ts
@@ -11,33 +11,39 @@ import {LOG} from "./logging/logger";
 import {apiRequestLogger} from "./logging/api-request-logger";
 import * as ENV from "./config/envConfig";
 
-
 const app: Express = express();
 
 const port: string = ENV.SERVER_PORT;
-const path: string = __dirname + "/views/";
+const viewPath: string = __dirname + "/views/";
+const distPath: string = __dirname + "/dist/";
 
-//get environemnt mode for local dev and set cors options
+// Get environment mode for local dev and set CORS options
 LOG.info("env mode: " + ENV.NODE_ENV);
 if(ENV.NODE_ENV === "dev"){
   app.use(cors(getCorstOptions()))
   // app.use(cors())
 }
 
-app.use("/sps-gui/", express.static(path));
-app.use(bodyParser.json());
-app.use(apiRequestLogger);
+app.use(express.static(viewPath));
+app.use('/sps-gui', express.static(distPath));
 
+app.use(bodyParser.json());
+
+app.use(apiRequestLogger);
 app.use(authorizationRoutes);
 app.use(routes);
 
-
+// Serve the index.html for the root path
 app.get("/", (req: Request, res: Response) => {
-  res.sendFile(path + "index.html");
+  res.sendFile(viewPath + "index.html");
+});
+
+app.get("/sps-gui/*", (req: Request, res: Response) => {
+  res.sendFile(distPath + "index.html");
 });
 
 app.get("*", (req: Request, res: Response) => {
-  res.sendFile(path + "index.html");
+  res.sendFile(viewPath + "index.html");
 });
 
 app.listen(port, () => {
