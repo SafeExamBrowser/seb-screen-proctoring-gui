5bbe8adb3ccffd732b6f72b4e5fe81ec0696802c
Nadim Ritter <nadim.ritter@let.ethz.ch>
2024-08-23 10:38:54 +0200

    revert some changes

--- server/src/app.ts
+++ server/src/app.ts
@@ -11,32 +11,43 @@ import {LOG} from "./logging/logger";
 import {apiRequestLogger} from "./logging/api-request-logger";
 import * as ENV from "./config/envConfig";
 
-
 const app: Express = express();
 
 const port: string = ENV.SERVER_PORT;
-const path: string = __dirname + "/views/";
+const viewPath: string = __dirname + "/views/";
+const distPath: string = __dirname + "/dist/";
 
-//get environment mode for local dev and set cors options
+// Get environment mode for local dev and set CORS options
 LOG.info("env mode: " + ENV.NODE_ENV);
 if(ENV.NODE_ENV === "dev"){
   app.use(cors(getCorstOptions()))
 }
 
-app.use(express.static(path));
-app.use(bodyParser.json());
-app.use(apiRequestLogger);
+//server static files
+app.use(express.static(viewPath));
+app.use(ENV.BASE_PATH, express.static(distPath));
 
+//parse incoming requests
+app.use(bodyParser.json());
+
+//middleware and routes config
+app.use(apiRequestLogger);
 app.use(authorizationRoutes);
 app.use(routes);
 
-
+// Serve the index.html for the root path
 app.get("/", (req: Request, res: Response) => {
-  res.sendFile(path + "index.html");
+  res.sendFile(viewPath + "index.html");
 });
 
+// Serve the index.html for custom path
+app.get(ENV.BASE_PATH + "/*", (req: Request, res: Response) => {
+  res.sendFile(distPath + "index.html");
+});
+
+//fallback route, SPA routing
 app.get("*", (req: Request, res: Response) => {
-  res.sendFile(path + "index.html");
+  res.sendFile(viewPath + "index.html");
 });
 
 app.listen(port, () => {
