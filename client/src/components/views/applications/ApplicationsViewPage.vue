3c69dcee419b548df5949c5d45bb051e53bfaab5
Nadim Ritter <nadim.ritter@let.ethz.ch>
2024-09-25 15:10:48 +0200

    SEBSP-162 base code for applications view

--- client/src/components/views/applications/ApplicationsViewPage.vue
+++ client/src/components/views/applications/ApplicationsViewPage.vue
@@ -1,10 +1,27 @@
 <template>
+    applications work
 
 </template>
 
 
 
 <script setup lang="ts">
+    import { ref, onBeforeMount, watch } from "vue";
+    import { useAppBarStore, useTableStore, useLoadingStore } from "@/store/store";
+
+
+    //store
+    const appBarStore = useAppBarStore();
+    const tableStore = useTableStore();
+    const loadingStore = useLoadingStore();
+
+
+
+
+    onBeforeMount(async () => {
+        appBarStore.title = "Applications"
+    });
+
 
 </script>
 
