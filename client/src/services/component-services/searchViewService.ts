e40e7fd288acbfa1a9967eecbab9cbcecabe532c
Andreas Hefti <andreas.hefti@id.ethz.ch>
2026-03-17 09:09:47 +0100

    SEBSP-221

--- client/src/services/component-services/searchViewService.ts
+++ client/src/services/component-services/searchViewService.ts
@@ -78,6 +78,6 @@ export function openProctoringApplicationSearch(sessionId: string, metadataApp: 
     url.searchParams.set("metadataApp", metadataApp);
     url.searchParams.set("metadataWindow", metadataWindow);
 
-    openUrlInNewTab(url.toString());
+    openUrlInNewTabApplicationView(url.toString());
 }
 //==============================
