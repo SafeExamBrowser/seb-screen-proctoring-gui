e0190395264d76dc6d78619a85ab6c1871e6b4cf
Andreas Hefti <andreas.hefti@id.ethz.ch>
2026-03-17 09:51:27 +0100

    SEBSP-221

--- client/src/services/component-services/searchViewService.ts
+++ client/src/services/component-services/searchViewService.ts
@@ -74,9 +74,24 @@ export function openProctoringView(sessionId: string, timestamp?: string){
 }
 
 export function openProctoringApplicationSearch(sessionId: string, metadataApp: string, metadataWindow: string){
-    const url: URL = new URL(constants.PROCTORING_APPLICATION_SEARCH_ROUTE + "/" + sessionId);
-    url.searchParams.set("metadataApp", metadataApp);
-    url.searchParams.set("metadataWindow", metadataWindow);
+    let url: string =  constants.PROCTORING_APPLICATION_SEARCH_ROUTE + "/" + sessionId;
+    let query: string | undefined = undefined;
+
+    if(metadataApp){
+        query = "metadataApp=" + metadataApp;
+    }
+    if (metadataWindow) {
+        if (query) {
+            query = query + "&";
+        }
+        query = query + "metadataWindow=" + metadataWindow;
+    }
+    if (query) {
+        url = url + "?" + query;
+    }
+
+    // url.searchParams.set("metadataApp", metadataApp);
+    // url.searchParams.set("metadataWindow", metadataWindow);
     console.info("********** url = " + url.toString());
     openUrlInNewTab(url.toString());
 }
